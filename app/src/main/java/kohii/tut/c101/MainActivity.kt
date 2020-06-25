6814df83fe4429c1b828782a07ff361f0ed31254
eneim <1776230+eneim@users.noreply.github.com>
2020-06-25 18:05:02 +0900

    feat: video url and layout

--- app/src/main/java/kohii/tut/c101/MainActivity.kt
+++ app/src/main/java/kohii/tut/c101/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+  companion object {
+    const val videoUrl = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
